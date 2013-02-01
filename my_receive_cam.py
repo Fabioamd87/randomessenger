f8f743de21d58595b97d7d65333723d1f9aa8b74
Fabioamd87 <fabioamd87@gmail.com>
2013-02-01 14:50:08 +0100

    uploading files

---
+++ my_receive_cam.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python
+""" this is a chat client, it connect automatically to the server,
+but if you have open port on your firewall/router you can receive connection from other clients"""
+
+import sys, os
+import pygtk, gtk, gobject
+import pygst
+pygst.require("0.10")
+import gst
+
+import threading
+import select
+import Queue
+from socket import socket, AF_INET, SOCK_STREAM
+
+LOCAL_HOST = ''
+CHAT_PORT = 5000
+VIDEO_PORT = 5001
+
+
+class NewMessageSignal(gobject.GObject):
+    def __init__(self):
+        self.__gobject_init__()
+
+class Receiver(threading.Thread):
+    def __init__(self,new_message_signal):
+        threading.Thread.__init__(self)
+        self.running = True
+        self.client_mode = False
+        self.server_mode = False
+
+        self.new_message_signal = new_message_signal
+        
+        self.sock = socket(AF_INET,SOCK_STREAM)
+        self.sock.bind((LOCAL_HOST, CHAT_PORT))
+        self.sock.listen(1)
+
+    def run(self):
+        #now = datetime.datetime.now()
+        timeout = 1
+        self.inputs = [self.sock]
+        self.outputs = []
+
+        self.new_message_signal.emit('sys_message', 'Listening for connections...')
+    
+        self.connect('192.168.1.2') #gestire eccezioni
+        self.new_message_signal.emit('sys_message', 'Connected to the server')
+
+        while self.running:
+            try:
+                readable, writable, exceptional = select.select(self.inputs, self.outputs, self.inputs, timeout)
+            except EBADF:
+                print("il server si e' disconnesso?")
+                sock.close()
+            for s in readable:
+                if s is self.sock:
+                    if self.server_mode == False and self.client_mode == False: #ovvero siamo in attesa, non connessi
+                        self.connection, self.client_address = s.accept()
+                        self.new_message_signal.emit('sys_message', 'Connection established with '+ self.client_address[0])
+                        #connection.setblocking(0)
+                        self.inputs.append(self.connection)
+                        self.client_mode == True
+                        print('We are in server mode')
+
+                        # Give the connection a queue for data we want to send
+                        #message_queues[connection] = Queue.Queue()
+                    else:
+                        #data from our connection
+                        print('message')
+                        data = s.recv(1024)
+                        if data:
+                            message = data.decode('utf-8')
+                            if message[:4] == '/sys':
+                                print('sys message')
+                                self.new_message_signal.emit('sys_message', message[4:])
+                            else:
+                                print('message for our connection')
+                                self.new_message_signal.emit('new_message', message)
+                        else:
+                            print('no data from the server')
+                            s.close()
+                else:
+                    print('We are in server mode')
+                    #another sock? another connection?
+                    data = s.recv(1024)
+                    if data:
+                        print('message from partner connection')
+                        message = data.decode('utf-8')
+                        self.new_message_signal.emit('new_message', message)
+                    else:
+                        print('no data, partner disconnected?')
+                        s.close()
+        print('returning')
+
+    def stop(self):
+        #disconnettersi senza far crashare il server
+        for c in self.inputs:
+            c.close()
+        self.sock.close()
+        self.input = []
+        self.running = False
+        print('stopping thread...')
+
+    def connect(self, address):
+        self.new_message_signal.emit('sys_message', 'Connecting to '+ address+'...')
+        self.client_address = address
+        self.sock = socket(AF_INET,SOCK_STREAM)
+        self.sock.connect((self.client_address, CHAT_PORT)) #gestire l'eccezioni
+        self.client_mode = True
+        self.inputs.append(self.sock)
+
+    def send(self, message):
+        if self.client_mode:
+            self.sock.send(message) #controllare disconnessione
+        else:
+            self.connection.send(message)
+
+class Video():
+    def __init__(self, host, port):
+        self.player = gst.Pipeline("player")
+        tcpclientsrc = gst.element_factory_make("tcpclientsrc", "tcpclientsrc")
+        tcpclientsrc.set_property("host", host)
+        tcpclientsrc.set_property("port", port)
+        #multipartdemux??
+        colorspace = gst.element_factory_make("jpegdec")
+        sink = gst.element_factory_make("autovideosink", "video-output")
+        caps = gst.Caps("video/x-raw-yuv, width=320, height=240, framerate=20/1")
+        self.player.add(tcpclientsrc, colorspace, sink)
+        gst.element_link_many(tcpclientsrc, colorspace, sink)
+
+        bus = self.player.get_bus()
+        bus.add_signal_watch()
+        bus.enable_sync_message_emission()
+        #bus.connect("message", self.on_message)
+        #bus.connect("sync-message::element", self.on_sync_message)
+
+    def start(self):
+        self.player.set_state(gst.STATE_PLAYING)
+
+class Chat(gtk.Window):
+    def __init__(self,new_message_signal, receiver):
+        super(Chat, self).__init__()
+
+        self.receiving = False
+
+        self.set_default_size(480, 640)
+        self.receiver = receiver
+        self.connect("destroy", self.quit)
+        
+        self.vbox = gtk.VBox()
+        #self.movie_window = gtk.DrawingArea()
+        #vbox.pack_start(self.movie_window, expand=True, fill=True, padding=0)        
+        self.add(self.vbox)
+        self.view = gtk.TextView()
+        self.view.set_editable(False)
+        self.buffer = self.view.get_buffer()
+        self.iter_pos = self.buffer.get_start_iter()
+        scrolled_window=gtk.ScrolledWindow()
+        scrolled_window.set_policy(gtk.POLICY_AUTOMATIC,gtk.POLICY_AUTOMATIC)
+        scrolled_window.add(self.view)
+        self.vbox.add(scrolled_window)
+        self.entry=gtk.Entry()
+        self.send_button=gtk.Button("Send")
+        self.send_button.connect("clicked", self.on_send_clicked)
+        self.connect_button = gtk.Button("Connect")
+        self.connect_button.connect("clicked", self.on_connect_clicked)
+
+        self.hbox=gtk.HBox()
+        self.hbox.pack_start(self.entry, expand=True, fill=True, padding=0)
+        self.hbox.pack_start(self.send_button, expand=False, fill=False, padding=0)
+        self.hbox.pack_start(self.connect_button, expand=False, fill=False, padding=0)
+        self.vbox.pack_start(self.hbox, expand=False, fill=False, padding=0)   
+        self.show_all()
+
+        self.entry.grab_focus()
+
+        new_message_signal.connect('new_message', self.on_new_message)
+        new_message_signal.connect('sys_message', self.on_sys_message)
+
+        #self.video = Video()
+
+        if self.receiving:
+            self.video.start()
+
+    def on_key_press_event(self, widget, event):
+        keyname = gtk.gdk.keyval_name(event.keyval)
+        print "Key %s (%d) was pressed" % (keyname, event.keyval)
+        if event.keyval == 65293:
+            print('ENTER')
+            self.send_button.clicked()
+
+        if self.entry.has_focus:
+            print('focus')
+        else:
+            print('non focus')
+
+    def quit(self, widget):
+        self.receiver.stop()
+        gtk.main_quit()
+
+    def on_send_clicked(self, widget):
+        text = self.entry.get_text()
+        self.receiver.send(text)
+        self.buffer.insert(self.iter_pos,'You: '+text+'\n') #unificare
+        self.entry.set_text('')
+
+    def on_connect_clicked(self, widget):
+        address = self.entry.get_text()
+        self.receiver.connect(address)
+
+    def on_new_message(self, widget, mess):
+        self.buffer.insert(self.iter_pos, 'Partner: '+mess+'\n') #unificare
+
+    def on_sys_message(self, widget, mess):
+        self.buffer.insert(self.iter_pos, mess+'\n')
+    
+    def on_message(self, bus, message):
+	    t = message.type
+	    if t == gst.MESSAGE_EOS:
+		    self.player.set_state(gst.STATE_NULL)
+	    elif t == gst.MESSAGE_ERROR:
+		    self.player.set_state(gst.STATE_NULL)
+		    err, debug = message.parse_error()
+		    print "Error: %s" % err, debug
+
+    def on_sync_message(self, bus, message):
+        if message.structure is None:
+            return
+        message_name = message.structure.get_name()
+        if message_name == "prepare-xwindow-id":
+            imagesink = message.src
+            imagesink.set_property("force-aspect-ratio", True)
+            gtk.gdk.threads_enter()
+            imagesink.set_xwindow_id(self.movie_window.window.xid)
+            gtk.gdk.threads_leave()
+
+gobject.threads_init()
+
+gobject.type_register(NewMessageSignal)
+gobject.signal_new("new_message", NewMessageSignal, gobject.SIGNAL_RUN_FIRST, gobject.TYPE_NONE, (gobject.TYPE_STRING,))
+gobject.signal_new("sys_message", NewMessageSignal, gobject.SIGNAL_RUN_FIRST, gobject.TYPE_NONE, (gobject.TYPE_STRING,))
+
+new_message_signal = NewMessageSignal()
+receiver = Receiver(new_message_signal)
+
+c = Chat(new_message_signal, receiver)
+c.connect('key_press_event', c.on_key_press_event)
+
+receiver.start()
+gtk.main()
